ffb249db378ab7c121037536714e1d1bfa8e4269
Allan19Prieto <allanbanilla@gmail.com>
2021-10-27 09:22:28 -0600

    Carpeta del servidor

---
+++ src/main/java/calserver/CalServer.java
@@ -0,0 +1,12 @@
+package calserver;
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/*Servidor principal de web socket*/
+@ServerEndpoint("/ws")
+public class CalServer {
+
+}
